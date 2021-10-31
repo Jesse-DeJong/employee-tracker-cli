b12cc0c61b99794fd89d7846e2cce205b83fbd77
Jesse De Jong <jdoceau@gmail.com>
2021-11-01 09:07:33 +1100

    view all employees func

--- server.js
+++ server.js
@@ -92,9 +92,29 @@ function viewAllRoles() {
       ORDER BY role.id ASC`, function (error, results) {
           console.table('\n', results, '\n');
           init();
-      });
+      })
 }
 
+function viewAllEmployees() {
+  db.query(`
+    SELECT  employee.id AS ID,
+            employee.first_name AS "FIRST NAME",
+            employee.last_name AS "LAST NAME",
+            role.title AS "TITLE",
+            role.salary AS "SALARY",
+            department.name AS "DEPARTMENT",
+            CONCAT(manager.first_name, " ", manager.last_name) AS "MANAGER NAME"
+              FROM employee
+                LEFT JOIN employee manager ON manager.id = employee.manager_id
+                INNER JOIN role ON employee.role_id = role.id
+                INNER JOIN department ON role.department_id = department.id`, 
+                  function (error, results) {
+                    console.table('\n', results, '\n');
+                    init();
+                  })
+}
+
+// Initialisation Call
 init();
 
 // sync sequelize models to the database, then turn on the server
